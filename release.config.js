f826e6dd20c1b3169bf9b740a0496f321ec5eefd
Krishna Modepalli <90754999+krishnamodepalli@users.noreply.github.com>
2024-09-23 23:31:56 +0530

    chore: update `release` workflow for github actions

--- release.config.js
+++ release.config.js
@@ -4,7 +4,8 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/git'
+    '@semantic-release/git',
+    '@semantic-release/github',
   ]
 };
 
